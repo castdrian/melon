b86e37cd49ff5b5f4f77b20188acfcc7aa0aa99c
castdrian <adrifcastr@gmail.com>
2024-01-07 22:23:35 +0100

    feat: content and delete flags for twitter linking

--- src/util/twitter.ts
+++ src/util/twitter.ts
@@ -1,6 +1,11 @@
 import { Scraper } from '@the-convocation/twitter-scraper';
 import { type BufferResolvable, type Message, time } from 'discord.js';
 
+enum ResponseFlags {
+  SHOW_CONTENT = '-c',
+  DELETE_MESSAGE = '-d',
+}
+
 export async function scrapeTweet(tweetId: string, message: Message) {
   try {
     const tweet = await new Scraper().getTweet(tweetId);
@@ -23,11 +28,19 @@ export async function scrapeTweet(tweetId: string, message: Message) {
       return;
     }
 
+    const shouldShowContent = message.content.toLowerCase().includes(ResponseFlags.SHOW_CONTENT);
+    const shouldDeleteMessage = message.content.toLowerCase().includes(ResponseFlags.DELETE_MESSAGE);
+
     const content = `Posted ${time(tweet.timestamp!, 'R')} by [@${tweet.username}](<https://twitter.com/${
       tweet.username
-    }>):\n\n${tweet.text}`;
-    await message.suppressEmbeds(true);
-    await message.reply({ content, files: attachments });
+    }>)${shouldShowContent ? `:\n\n${tweet.text}` : ''}`;
+
+    if (shouldDeleteMessage) {
+      await message.channel.send({ content, files: attachments });
+      await message.delete().catch(() => null);
+    } else {
+      await message.reply({ content, files: attachments });
+    }
   } catch (error) {
     message.client.logger.error(`An error occurred in scrapeTweet: ${error}`);
   }
