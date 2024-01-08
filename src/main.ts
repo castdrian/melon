627c75664111379da4a00833d2d05dad131830d1
castdrian <adrifcastr@gmail.com>
2024-01-08 16:53:47 +0100

    chore: temporarily comment this out

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,8 @@ const client = new SapphireClient({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
-const sqlite = new Database(config.devGuildId ? ':memory:' : 'melon.db', { create: true });
-const db = drizzle(sqlite);
-migrate(db, { migrationsFolder: './src/database/drizzle' });
+// const sqlite = new Database(config.devGuildId ? ':memory:' : 'melon.db', { create: true });
+// const db = drizzle(sqlite);
+// migrate(db, { migrationsFolder: './src/database/drizzle' });
 
 await client.login(config.discordToken);
