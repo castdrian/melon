7cead8eff5a7932553bf7f69b0f962a7cdbc9b95
castdrian <adrifcastr@gmail.com>
2024-01-08 18:06:27 +0100

    chore: update db schema

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,7 +1,5 @@
-// import { Database } from 'bun:sqlite';
-// import { drizzle } from 'drizzle-orm/bun-sqlite';
+import { Database } from 'bun:sqlite';
+import { drizzle } from 'drizzle-orm/bun-sqlite';
 
-// import { config } from '@src/config';
-
-// const sqlite = new Database(config.devGuildId ? ':memory:' : 'melon.db', { create: true });
-// export const db = drizzle(sqlite);
+const sqlite = new Database('melon.db');
+export const db = drizzle(sqlite);
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -2,8 +2,8 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const guildPreferences = sqliteTable('guild_preferences', {
   id: text('id').primaryKey(),
-  twitterAutoEmbed: integer('twitter_auto_embed').default(0),
-  instagramAutoEmbed: integer('instagram_auto_embed').default(0),
+  twitterAutoEmbed: integer('twitter_auto_embed', { mode: 'boolean' }).notNull().default(false),
+  instagramAutoEmbed: integer('instagram_auto_embed', { mode: 'boolean' }).notNull().default(false),
   greetingChannelId: text('greeting_channel_id'),
   greetingMessage: text('greeting_message'),
 });
