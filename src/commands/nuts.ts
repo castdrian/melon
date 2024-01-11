de20410bc8c05994982530c3e8b4a1c4ab9fcac4
castdrian <adrifcastr@gmail.com>
2024-01-11 18:22:07 +0100

    feat: add ryujindn

--- src/commands/nuts.ts
+++ src/commands/nuts.ts
@@ -8,7 +8,10 @@ export class DeezNutsCommand extends Command {
     try {
       if (!interaction.isMessageContextMenuCommand && !(interaction.targetMessage instanceof Message)) return;
       const __dirname = path.dirname(new URL(import.meta.url).pathname);
-      const filePath = path.join(__dirname, '..', '..', 'media', 'DEEZNUTS.mov');
+      const one = path.join(__dirname, '..', '..', 'media', 'DEEZNUTS.mov');
+      const two = path.join(__dirname, '..', '..', 'media', 'RYUJINDN.mp4');
+
+      const filePath = Math.random() > 0.5 ? one : two;
 
       await interaction.reply({ content: interaction.targetMessage.author.toString(), files: [filePath] });
     } catch (ex) {
