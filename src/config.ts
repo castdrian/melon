c59dd78ecd969fc42dd49914beaa8bf8ed447584
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2025-05-07 16:11:21 +0200

    fix: neato config config needed new config

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,17 @@
-import { createConfigLoader } from "@neato/config";
-import { z } from 'zod';
+import { createConfig, loaders } from "@neato/config";
+import { z } from "zod";
 
 const schema = z.object({
-  discordToken: z.string().min(1),
-  devGuildId: z.string().optional(),
-  instagramApiToken: z.string().min(1),
+	discordToken: z.string().min(1),
+	devGuildId: z.string().optional(),
+	instagramApiToken: z.string().min(1),
 });
 
-const prefix = 'CONF_';
-
-export const config = createConfigLoader()
-  .addFromFile('.env', { prefix })
-  .addFromEnvironment(prefix)
-  .addZodSchema(schema)
-  .load();
+export const config = createConfig({
+	schema,
+	envPrefix: "CONF_",
+	loaders: [loaders.file(".env"), loaders.environment()],
+	freeze: true,
+});
 
 export const MELON_COLOR = 0xd23b68;
